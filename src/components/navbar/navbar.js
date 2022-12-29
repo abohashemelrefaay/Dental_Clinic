53282b04ab43d6f57cd96111ceaa38228f41ecd0
abohashemelrefaay <abohashemelrefaay@gmail.com>
2022-12-29 20:27:46 +0200

    naw

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './style_navbar.css';
 
 const Navbar = () => {
+  const [classchange,setclasschange]=useState("links");
+  document.addEventListener("click",(e)=>{
+    if(e.target!== document.querySelector(".newclass") && e.target !== document.querySelector(".icon") ){
+      setclasschange("links")
+    }
+  });
+  const handel_click_icon=()=>{
+    if(classchange==="links"){
+      setclasschange("newclass")
+    }else if(classchange==="newclass"){
+      setclasschange("links")
+    }
+  }
   return (
     <div className='navbar'>
+    
+      <span className="icon" onClick={ handel_click_icon}>
+              <span></span>
+              <span></span>
+              <span></span>
+           </span>
       <div className="container">
        <div className="nav_cover">
         <div className="logo">OralForm</div>
-        <ul className='links'>
-          <li><a href='/'>Home</a></li>
+        <ul className={classchange}>
+          <li><a  href='/'>Home</a></li>
           <li><a href='/About_Us'>About Us</a></li>
           <li><a href='/Services'>Services </a></li>
           <li><a href='/Our_mission'>Our mission</a></li>
