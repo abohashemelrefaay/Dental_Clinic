6a8da63c6e52c05ccdb57d6fd6baa954dcfffb76
abohashemelrefaay <abohashemelrefaay@gmail.com>
2022-12-29 16:37:53 +0200

    naw

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,28 +1,46 @@
-import React from 'react';
-import './style_navbar.css';
+import React from "react";
+import "./style_navbar.css";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="container">
-       <div className="nav_cover">
-        <div className="logo">OralForm</div>
-        <ul className='links'>
-          <li><a href='/'>Home</a></li>
-          <li><a href='/About_Us'>About Us</a></li>
-          <li><a href='/Services'>Services </a></li>
-          <li><a href='/Our_mission'>Our mission</a></li>
-          <li><a href='/Contact'>Contact</a></li>
-        </ul>
-      <form className='login'>
-        <input type="submit" onClick={(o)=>o.preventDefault()} className='login_submit' value="Login as Patient" />
-        <input type="submit" onClick={(o)=>o.preventDefault()} className='login_submit' value="Login as Dentist" />
-      </form>
-       </div>
+          <div className="logo">OralForm</div>
+          <div className="links">
+            <span>
+              <a href="/">Home</a>
+            </span>
+            <span>
+              <a href="/About_Us">About Us</a>
+            </span>
+            <span>
+              <a href="/Services">Services </a>
+            </span>
+            <span>
+              <a href="/Our_mission">Our mission</a>
+            </span>
+            <span>
+              <a href="/Contact">Contact</a>
+            </span>
+          </div>
 
+          <div className="login">
+            <input
+              type="submit"
+              onClick={(o) => o.preventDefault()}
+              className="login_submit"
+              value="Login as Patient"
+            />
+            <input
+              type="submit"
+              onClick={(o) => o.preventDefault()}
+              className="login_submit"
+              value="Login as Dentist"
+            />
+          </div>
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
